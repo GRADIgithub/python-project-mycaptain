b1a56072426ee867c518d98e2294a579f88c1ed7
GRADIgithub <100024988+GRADIgithub@users.noreply.github.com>
2022-05-20 12:42:13 +0530

    Update radius of circle.py

--- radius of circle.py
+++ radius of circle.py
@@ -1,3 +1,7 @@
 from math import pi
+
+#instead of put pi value import pi from math lib:
+
 radius = float(input ("Input the radius of the circle : "))
-print ("The area of the circle with radius " + str(radius) + " is: " + str(pi * radius**2))
+
+print ("The area of the circle with radius " ,(radius), " is: " ,(pi * radius**2))
